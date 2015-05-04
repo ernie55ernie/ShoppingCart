38b009aaf922d935e056f795fbc9f40464877c78
ernie <b01505025@ntu.edu.tw>
2015-05-04 16:19:58 +0800

    2015/5/4 Parse String to a Rule with a single consequent. Rule repeating haven't been checked.

--- src/model/rule/RuleBase.java
+++ src/model/rule/RuleBase.java
@@ -7,16 +7,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.commons.collections4.map.MultiKeyMap;
 
 import model.cart.ShoppingCart;
 import model.entity.Customer;
 import model.entity.Product;
 import model.facade.CustomerFacade;
+import model.facade.ProductFacade;
+
+import org.apache.commons.collections4.map.MultiKeyMap;
 
 /**
  * {@link RuleBase} stores inference {@link Rule} from {@link ShoppingCart}} data using {@link Map}.
@@ -63,9 +65,18 @@ public class RuleBase {
 		
 		int customerId = Integer.parseInt(shoppingCartArray[0].substring(1));
 		Customer customer = CustomerFacade.getInstance().find(customerId);
-		int currentDivision;
-		for(currentDivision = 0; currentDivision < length; currentDivision++){
-			
+		
+		ProductFacade productFacade = ProductFacade.getInstance();
+		int currentItem;
+		for(currentItem = 0; currentItem < length; currentItem++){
+			List<Product> antecedent = new ArrayList<Product>();
+			List<Product> consequent = new ArrayList<Product>();
+			for(String aProduct: shoppingCartArray){
+				antecedent.add(
+						productFacade.find(Integer.parseInt(aProduct.substring(1))));
+			}
+			consequent.add(antecedent.remove(currentItem));
+			Rule rule = new Rule(customer, antecedent, consequent);
 		}
 	}
 	
